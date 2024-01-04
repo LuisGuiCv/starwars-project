c872aaf23ed526ec6a73f941ef93f70683a4328b
LuisGuiCv <luisguillermocruzvargas99@gmail.com>
2024-01-03 22:02:58 -0600

    project upload

---
+++ src/main/java/com/starwars/project/StarWarsApiProject/StarWarsApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.starwars.project.StarWarsApiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starwars/project/StarWarsApiProject/StarWarsApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwars.project.StarWarsApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
